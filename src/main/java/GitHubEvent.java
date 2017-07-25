f7e3b60e6c93bbe1053bc16079f98e9313a7ede5
Ali Rezazadeh <Ali.Rzadeh75@gmail.com>
2017-07-25 15:33:28 +0430

    Initial commit

---
+++ src/main/java/GitHubEvent.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Ali on 7/25/17.
+ */
+public class GitHubEvent {
+
+    String id;
+
+    String type;
+
+    GitHubEvent() {}
+
+    GitHubEvent(String id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+
+}
---
+++ src/main/java/SubscribeToOpenChannel.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Ali on 7/25/17.
+ */
+
+import com.satori.rtm.*;
+import com.satori.rtm.model.*;
+
+public class SubscribeToOpenChannel {
+
+    static final String endpoint = "wss://open-data.api.satori.com";
+    static final String appkey = "783ecdCcb8c5f9E66A56cBFeeeB672C3";
+    static final String channel = "github-events";
+
+    public static void main(String[] args) throws InterruptedException {
+        final RtmClient client = new RtmClientBuilder(endpoint, appkey)
+                .setListener(new RtmClientAdapter() {
+                    @Override
+                    public void onEnterConnected(RtmClient client) {
+                        System.out.println("Connected to Satori RTM!");
+                    }
+                })
+                .build();
+
+
+        SubscriptionAdapter listener = new SubscriptionAdapter() {
+            @Override
+            public void onSubscriptionData(SubscriptionData data) {
+                for (AnyJson json : data.getMessages()) {
+
+//                    System.out.println("Got message: " + json.toString());
+                    GitHubEvent event = json.convertToType(GitHubEvent.class);
+                    System.out.println(event.id);
+
+                }
+            }
+        };
+
+        client.createSubscription(channel, SubscriptionMode.SIMPLE, listener);
+
+        client.start();
+    }
+
+}
