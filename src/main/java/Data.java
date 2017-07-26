8ccc5f7bb4d6883f7f0afd2d1013ce773840ea28
Ali Rezazadeh <Ali.Rzadeh75@gmail.com>
2017-07-26 15:08:01 +0430

    add mostFrequentActor

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 public class Data {
 
     static private HashMap<String, Integer> repoFrequency = new HashMap();
+    static private HashMap<String, Integer> actorFrequency = new HashMap();
 
     public static void addRepoID(String ID) {
         if(repoFrequency.containsKey(ID))
@@ -16,15 +17,33 @@ public class Data {
             repoFrequency.put(ID, 1);
     }
 
-    public static String getMost() {
-        int max = 1;
+    public static String getMostFrequentRepo() {
+        int max = 0;
         String mID = "";
         for (String ID: repoFrequency.keySet())
-            if(repoFrequency.get(ID) > max) {
+            if(repoFrequency.get(ID) >= max) {
                 max = repoFrequency.get(ID);
                 mID = ID;
             }
         return "RepoID : " + mID + " frequency = " + max;
+    }
+
+    public static void addActorID(String ID) {
+        if(actorFrequency.containsKey(ID))
+            actorFrequency.put(ID, 1 + actorFrequency.get(ID));
+        else
+            actorFrequency.put(ID, 1);
+    }
+
+    public static String getMostFrequentActor() {
+        int max = 0;
+        String mID = "";
+        for (String ID: actorFrequency.keySet())
+            if(actorFrequency.get(ID) >= max) {
+                max = actorFrequency.get(ID);
+                mID = ID;
+            }
+        return "ActorID : " + mID + " frequency = " + max;
     }
 
 }
--- src/main/java/SubscribeToOpenChannel.java
+++ src/main/java/SubscribeToOpenChannel.java
@@ -5,9 +5,8 @@
 import com.satori.rtm.*;
 import com.satori.rtm.model.*;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class SubscribeToOpenChannel {
 
@@ -15,13 +14,14 @@ public class SubscribeToOpenChannel {
     static final String appkey = "783ecdCcb8c5f9E66A56cBFeeeB672C3";
     static final String channel = "github-events";
 
-    static private Queue<AnyJson> messages = new LinkedList<AnyJson>();
+    static private BlockingQueue<AnyJson> jsonMessages = new LinkedBlockingQueue<AnyJson>();
+    static private BlockingQueue<GitHubEvent> eventObjects = new LinkedBlockingQueue<GitHubEvent>();
     static private Analyser analyser = new Analyser();
-    static private boolean flag = true;
+    static private Parser parser = new Parser();
+    static private boolean flag1 = false;
+    static private boolean flag2 = false;
 
     public static void main(String[] args) throws InterruptedException {
-        final long startTime = System.currentTimeMillis();
-        System.out.println(startTime);
         final RtmClient client = new RtmClientBuilder(endpoint, appkey)
                 .setListener(new RtmClientAdapter() {
                     @Override
@@ -31,16 +31,28 @@ public class SubscribeToOpenChannel {
                 })
                 .build();
 
+        final long startTime = System.currentTimeMillis();
         SubscriptionAdapter listener = new SubscriptionAdapter() {
+            int i = 0;
+            int j = 0;
             @Override
             public void onSubscriptionData(SubscriptionData data) {
+
+                System.out.println("num of data packets = "+ ++i);
                 for (AnyJson json : data.getMessages()) {
-                        messages.add(json);
-                        long currentTime = System.currentTimeMillis();
-                        if(currentTime - startTime > 1*60*1000) {
-                            flag = false;
-                            break;
-                        }
+                    System.out.println("num of messages = "+ ++j);
+                    try {
+                        jsonMessages.put(json);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                long currentTime = System.currentTimeMillis();
+                if(currentTime - startTime > 5*60*1000) {
+                    flag1 = true;
+                    client.shutdown();
+                    System.out.println("stops");
+                    parser.interrupt();
                 }
             }
         };
@@ -48,20 +60,57 @@ public class SubscribeToOpenChannel {
         client.createSubscription(channel, SubscriptionMode.SIMPLE, listener);
 
         analyser.start();
+        parser.start();
         client.start();
+    }
+
+    static private class Parser extends Thread {
+
+        public void run(){
+            while(!(flag1 && jsonMessages.isEmpty())){
+                AnyJson json;
+                try {
+                    json = jsonMessages.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    if(jsonMessages.isEmpty() && flag1)
+                        break;
+                    else
+                        continue;
+                }
+                System.out.println("after take 1");
+                GitHubEvent event = json.convertToType(GitHubEvent.class);
+                try {
+                    eventObjects.put(event);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            flag2 = true;
+            analyser.interrupt();
+        }
     }
 
     static private class Analyser extends Thread {
 
         public void run(){
-            while(!messages.isEmpty() || flag){
-                AnyJson json = messages.poll();
-                if(json == null)
-                    continue;
-                GitHubEvent event = json.convertToType(GitHubEvent.class);
+            while(!(flag2 && eventObjects.isEmpty())){
+                GitHubEvent event = null;
+                try {
+                    event = eventObjects.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    if(eventObjects.isEmpty() && flag1)
+                        break;
+                    else
+                        continue;
+                }
+                System.out.println("after take 2");
                 Data.addRepoID(event.repo.id);
+                Data.addActorID(event.actor.id);
             }
-            System.out.println(Data.getMost());
+            System.out.println(Data.getMostFrequentRepo());
+            System.out.println(Data.getMostFrequentActor());
         }
     }
 
